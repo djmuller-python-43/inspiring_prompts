3159805770fbf2038f94dee55795ea4e005b61c2
Duane Muller <djmuller.python.43@gmail.com>
2023-07-14 11:54:56 +0200

    fixed edit button

---
+++ app/api/prompt/route.jsx
@@ -0,0 +1,13 @@
+import { connectDB } from "@utils/database"
+import Prompt from "@models/prompt"
+
+export const GET = async (req) => {
+  try {
+    await connectDB()
+
+    const prompts = await Prompt.find({}).populate('creator')
+    return new Response(JSON.stringify(prompts), { status: 200 })
+  } catch (error) {
+    return new Response('Failed to fetch any prompts', { status: 500 })
+  }
+}
---
+++ app/profile/page.jsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const [prompts, setPrompts] = useState([]);
+
+  useEffect(() => {
+    const fetchPrompts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/prompts`);
+      const data = await response.json();
+
+      setPrompts(data);
+    };
+
+    if (session?.user.id) fetchPrompts();
+  }, [session?.user.id]);
+
+  const handleEdit = (prompt) => {
+    router.push(`/update-prompt?id=${prompt._id}`);
+  };
+
+  const handleDelete = async (prompt) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${prompt._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPrompts = prompts.filter((item) => item._id !== prompt._id);
+
+        setMyPosts(filteredPrompts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  return (
+    <Profile
+      name='My'
+      desc='Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination'
+      data={prompts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
+
+export default MyProfile;
