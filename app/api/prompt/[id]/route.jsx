2424dde2a55a0e5a40bead87cd51c364ab0cc47d
Duane Muller <djmuller.python.43@gmail.com>
2023-07-14 16:27:50 +0200

    fixed another spelling error

--- app/api/prompt/[id]/route.jsx
+++ app/api/prompt/[id]/route.jsx
@@ -19,7 +19,7 @@ export const GET = async (req, { params }) => {
 //PATCH
 
 export const PATCH = async (req, { params }) => {
-  const { prompt, tag } = await request.json()
+  const { prompt, tag } = await req.json()
 
   try {
     await connectDB()
